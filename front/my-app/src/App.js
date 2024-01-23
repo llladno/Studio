7c06fd04332376f85526fa693a3dcdb0b5587a46
llladno <man30968@gmail.com>
2024-01-23 10:54:54 +0300

    commit

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -17,15 +17,15 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path='/studio' element={<Home></Home>}></Route>
-                <Route path='/studio/login' element={<Login></Login>}></Route>
-                <Route path='/studio/login/home' element={<LoginHome></LoginHome>}></Route>
-                <Route path='/studio/login/order' element={<LoginOrder></LoginOrder>}></Route>
-                <Route path='/studio/login/register' element={<LoginRegister></LoginRegister>}></Route>
-                <Route path='/studio/home/photographer' element={<PagePhotographer></PagePhotographer>}></Route>
-                <Route path='/studio/login/photos' element={<LoginPhotos></LoginPhotos>}></Route>
-                <Route path='/studio/develop' element={<Develop/>}></Route>
-                <Route path='/studio/studio' element={<StudioPage/>}></Route>
+                <Route path='./studio' element={<Home></Home>}></Route>
+                <Route path='./studio/login' element={<Login></Login>}></Route>
+                <Route path='./studio/login/home' element={<LoginHome></LoginHome>}></Route>
+                <Route path='./studio/login/order' element={<LoginOrder></LoginOrder>}></Route>
+                <Route path='./studio/login/register' element={<LoginRegister></LoginRegister>}></Route>
+                <Route path='./studio/home/photographer' element={<PagePhotographer></PagePhotographer>}></Route>
+                <Route path='./studio/login/photos' element={<LoginPhotos></LoginPhotos>}></Route>
+                <Route path='./studio/develop' element={<Develop/>}></Route>
+                <Route path='./studio/studio' element={<StudioPage/>}></Route>
             </Routes>
         </div>
     );
